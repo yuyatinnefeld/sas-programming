f506e09b76a15f7dabc09fe4d94fded02a938fee
Yuya <yuyatinnefeld@gmail.com>
2021-06-30 12:38:06 +0200

    initial setup

--- project/crawl.py
+++
@@ -1,9 +0,0 @@
-import graphene
-import extraction
-import requests
-
-
-def extract(url):
-    html = requests.get(url).text
-    extracted = extraction.Extractor().extract(html, source_url=url)
-    return extracted
